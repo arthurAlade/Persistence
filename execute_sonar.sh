b7a732198826e1149ac1432f765a506af71aa5aa
Pradityo Adi WICAKSONO <pradityo.wicaksono@yahoo.com>
2023-10-04 17:36:42 +0200

    add sonar configuration

---
+++ execute_sonar.sh
@@ -0,0 +1,4 @@
+mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=fr.uga.miage.m1:mg1_2 \
+  -Dsonar.host.url=http://im2ag-sonar.u-ga.fr:9000 \
+  -Dsonar.login=7061e850d47367d4e3981e893edc9874b2d9e601
