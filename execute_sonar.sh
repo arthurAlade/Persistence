7dd571c0d4b02ee5a1e0dd9748f670160b25058a
Pradityo Adi WICAKSONO <pradityo.wicaksono@yahoo.com>
2023-10-05 10:01:43 +0200

    project files restructuration -- sonar works

--- execute_sonar.sh
+++ execute_sonar.sh
@@ -1,4 +1,4 @@
 mvn clean verify sonar:sonar \
-  -Dsonar.projectKey=fr.uga.miage.m1:mg1_2 \
+  -Dsonar.projectKey=fr.uga.miage.m1:Persistence_g1_2 \
   -Dsonar.host.url=http://im2ag-sonar.u-ga.fr:9000 \
-  -Dsonar.login=7061e850d47367d4e3981e893edc9874b2d9e601
+  -Dsonar.login=8b4e79732a0923045ccdadc5cb90337f2577803d
