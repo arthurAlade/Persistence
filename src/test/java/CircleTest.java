91ee2f39285bf11c5c2402823a35f6b7b8815a6f
Pradityo Adi WICAKSONO <pradityo.wicaksono@yahoo.com>
2023-10-17 11:26:34 +0200

    start of tests

---
+++ src/test/java/CircleTest.java
@@ -0,0 +1,100 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Random;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import edu.uga.miage.m1.polygons.gui.persistence.JSonVisitor;
+import edu.uga.miage.m1.polygons.gui.persistence.Visitor;
+import edu.uga.miage.m1.polygons.gui.shapes.Circle;
+
+public class CircleTest {
+    @Test
+    @DisplayName("Create circle with min int value")
+    public void createCircleMinInt() {
+        int coord = Integer.MIN_VALUE;
+        Circle circle = new Circle(coord, coord);
+
+
+        assertEquals(coord-25, circle.getX());
+        assertEquals(coord-25, circle.getY());
+    }
+
+    @Test
+    public void testCreateNewCircle() {
+        // Create a new Circle object with the x and y coordinates set to 100.
+        Circle circle = new Circle(100, 100);
+
+        // Assert that the Circle object was created successfully.
+        assertNotNull(circle);
+
+        // Assert that the Circle object has the correct x and y coordinates.
+        assertEquals(100-25, circle.getX());
+        assertEquals(100-25, circle.getY());
+    }
+
+    @Test
+    @DisplayName("Create circle with 0 value")
+    public void createCircleZeroCoords() {
+        int coord = 0;
+        Circle circle = new Circle(coord, coord);
+
+
+        assertEquals((circle.getX()), coord-25);
+        assertEquals((circle.getY()), coord-25);
+    }
+
+
+    @Test
+    @DisplayName("Create circle with max int value")
+    public void createCircleMaxInt() {
+        int coord = Integer.MAX_VALUE;
+        Circle circle = new Circle(coord, coord);
+
+
+        assertEquals(coord-25, circle.getX());
+        assertEquals(coord-25, circle.getY());
+    }
+
+    @Test
+    @DisplayName("Create circle with random coord")
+    public void createCircleRandoomCoord() {
+        Random randomX = new Random();
+        Random randomY = new Random();
+
+        int randomNumberX = randomX.nextInt();
+        int randomNumberY = randomY.nextInt();
+
+        Circle circle = new Circle(randomNumberX, randomNumberY);
+
+        assertEquals(randomNumberX-25, circle.getX());
+        assertEquals(randomNumberY-25, circle.getY());
+    }
+
+    @Test
+    @DisplayName("Visiting circle with Json visitor")
+    public void visitingACricle() {
+        Random randomX = new Random();
+        Random randomY = new Random();
+
+        int randomNumberX = randomX.nextInt();
+        int randomNumberY = randomY.nextInt();
+
+        Circle circle = new Circle(randomNumberX, randomNumberY);
+
+        Visitor visitor = new JSonVisitor();
+        visitor.visit(circle);
+
+        String result = "{\n" + "        \"type\": \"circle\",\n" + "        \"x\": " + (randomNumberX-25) + ",\n" + "        \"y\": " + (randomNumberY-25) + "\n" + "}";
+
+
+        assertEquals(visitor.getRepresentation(), result);
+    }
+
+    @Test
+    @DisplayName("draw a circle in graphic2D test")
+    public void drawTest() {
+
+
+
+    }
+}
---
+++ src/test/java/TriangleTest.java
@@ -0,0 +1,100 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Random;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import edu.uga.miage.m1.polygons.gui.persistence.JSonVisitor;
+import edu.uga.miage.m1.polygons.gui.persistence.Visitor;
+import edu.uga.miage.m1.polygons.gui.shapes.Triangle;
+
+public class TriangleTest {
+    @Test
+    @DisplayName("Create triangle with min int value")
+    public void createTriangleMinInt() {
+        int coord = Integer.MIN_VALUE;
+        Triangle triangle = new Triangle(coord, coord);
+
+
+        assertEquals(coord-25, triangle.getX());
+        assertEquals(coord-25, triangle.getY());
+    }
+
+    @Test
+    public void testCreateNewTriangle() {
+        // Create a new Triangle object with the x and y coordinates set to 100.
+        Triangle triangle = new Triangle(100, 100);
+
+        // Assert that the Triangle object was created successfully.
+        assertNotNull(triangle);
+
+        // Assert that the Triangle object has the correct x and y coordinates.
+        assertEquals(100-25, triangle.getX());
+        assertEquals(100-25, triangle.getY());
+    }
+
+    @Test
+    @DisplayName("Create triangle with 0 value")
+    public void createTriangleZeroCoords() {
+        int coord = 0;
+        Triangle triangle = new Triangle(coord, coord);
+
+
+        assertEquals((triangle.getX()), coord-25);
+        assertEquals((triangle.getY()), coord-25);
+    }
+
+
+    @Test
+    @DisplayName("Create triangle with max int value")
+    public void createTriangleMaxInt() {
+        int coord = Integer.MAX_VALUE;
+        Triangle triangle = new Triangle(coord, coord);
+
+
+        assertEquals(coord-25, triangle.getX());
+        assertEquals(coord-25, triangle.getY());
+    }
+
+    @Test
+    @DisplayName("Create triangle with random coord")
+    public void createTriangleRandoomCoord() {
+        Random randomX = new Random();
+        Random randomY = new Random();
+
+        int randomNumberX = randomX.nextInt();
+        int randomNumberY = randomY.nextInt();
+
+        Triangle triangle = new Triangle(randomNumberX, randomNumberY);
+
+        assertEquals(randomNumberX-25, triangle.getX());
+        assertEquals(randomNumberY-25, triangle.getY());
+    }
+
+    @Test
+    @DisplayName("Visiting triangle with Json visitor")
+    public void visitingACricle() {
+        Random randomX = new Random();
+        Random randomY = new Random();
+
+        int randomNumberX = randomX.nextInt();
+        int randomNumberY = randomY.nextInt();
+
+        Triangle triangle = new Triangle(randomNumberX, randomNumberY);
+
+        Visitor visitor = new JSonVisitor();
+        visitor.visit(triangle);
+
+        String result = "{\n" + "        \"type\": \"triangle\",\n" + "        \"x\": " + (randomNumberX-25) + ",\n" + "        \"y\": " + (randomNumberY-25) + "\n" + "}";
+
+
+        assertEquals(visitor.getRepresentation(), result);
+    }
+
+    @Test
+    @DisplayName("draw a triangle in graphic2D test")
+    public void drawTest() {
+
+
+
+    }
+}
