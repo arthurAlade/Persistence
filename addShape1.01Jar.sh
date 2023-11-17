7a2837051ae8e63fd3bc48e73c4dbc9fb51e3441
Pradityo Adi WICAKSONO <pradityo.wicaksono@yahoo.com>
2023-11-17 20:26:52 +0100

    fix wrong version in shape installation script

--- addShape1.01Jar.sh
+++ addShape1.01Jar.sh
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=lib/shape.jar -DgroupId=edu.uga.miage.m1 -DartifactId=edu.uga.singleshape -Dversion=1.0.1 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile="lib/shape-1.0.1.jar" -DgroupId="edu.uga.miage.m1" -DartifactId="edu.uga.singleshape" -Dversion="1.0.1" -Dpackaging=jar -DgeneratePom=true
