7d07e13c1538737c7eca4dedd942bc394228688c
Pradityo Adi WICAKSONO <pradityo.wicaksono@yahoo.com>
2023-11-18 13:25:22 +0100

    remove unused script

--- addShape1.01Jar.sh
+++
@@ -1,1 +0,0 @@
-mvn install:install-file -Dfile="lib/shape-1.0.1.jar" -DgroupId="edu.uga.miage.m1" -DartifactId="edu.uga.singleshape" -Dversion="1.0.1" -Dpackaging=jar -DgeneratePom=true
--- addShapeJar.sh
+++
@@ -1,1 +0,0 @@
-mvn install:install-file -Dfile=lib/shape.jar -DgroupId=edu.uga.miage.m1 -DartifactId=edu.uga.singleshape -Dversion=1.0.0 -Dpackaging=jar -DgeneratePom=true
