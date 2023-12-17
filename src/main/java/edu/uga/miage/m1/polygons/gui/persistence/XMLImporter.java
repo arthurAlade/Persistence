ef8605cc47efcd4e62a0523fca2bbd3c461f260b
Pradityo Adi WICAKSONO <pradityo.wicaksono@yahoo.com>
2023-12-17 19:06:57 +0100

    Disable access to external entities in XML parsing

--- src/main/java/edu/uga/miage/m1/polygons/gui/persistence/XMLImporter.java
+++ src/main/java/edu/uga/miage/m1/polygons/gui/persistence/XMLImporter.java
@@ -6,7 +6,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -17,10 +17,9 @@ import java.util.List;
 
 
 public class XMLImporter {
-    public XMLImporter() {
-    }
+    public XMLImporter() {}
 
-    public AbstractShape AbstractShapeGenerator(String shapeType, int x, int y){
+    public AbstractShape AbstractShapeGenerator(String shapeType, int x, int y) {
 
         return switch (shapeType) {
             case "circle" -> new Circle(x, y);
@@ -29,20 +28,23 @@ public class XMLImporter {
         };
     }
 
-    public AbstractShape CubeGenerator(String shape, int x, int y, int size){
-        return new Cube(x,y,size);
+    public AbstractShape CubeGenerator(String shape, int x, int y, int size) {
+        return new Cube(x, y, size);
     }
 
-    public AbstractShape GroupShapeGenerator(String shape, int x, int y, int xEnd, int yEnd, List<AbstractShape> shapes){
+    public AbstractShape GroupShapeGenerator(String shape, int x, int y, int xEnd, int yEnd,
+            List<AbstractShape> shapes) {
         return new GroupShape(x, y, xEnd, yEnd, shapes);
 
     }
 
-    public List<AbstractShape> importAbstractShape(String filename){
+    public List<AbstractShape> importAbstractShape(String filename) {
         List<AbstractShape> listShape = new ArrayList<AbstractShape>();
 
         File inputFile = new File(filename);
-        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newDefaultInstance();
+        dbFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+        dbFactory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
         try {
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(inputFile);
@@ -56,24 +58,30 @@ public class XMLImporter {
                     Element shapeElement = (Element) shapeNode;
 
                     // Get parse and extract type, x and y from XML file
-                    String type = shapeElement.getElementsByTagName("type").item(0).getTextContent();
-                    String xString = shapeElement.getElementsByTagName("x").item(0).getTextContent();
-                    String yString = shapeElement.getElementsByTagName("y").item(0).getTextContent();
+                    String type =
+                            shapeElement.getElementsByTagName("type").item(0).getTextContent();
+                    String xString =
+                            shapeElement.getElementsByTagName("x").item(0).getTextContent();
+                    String yString =
+                            shapeElement.getElementsByTagName("y").item(0).getTextContent();
                     int x = Integer.parseInt(xString);
                     int y = Integer.parseInt(yString);
 
-                    if(type.equals("cube")){
-                        int size = Integer.parseInt(shapeElement.getElementsByTagName("size").item(0).getTextContent());
-                        listShape.add(this.CubeGenerator(type, x+25, y+25, size));
-                    } else if(type.equals("groupShape")){
-                        String xEndString = shapeElement.getElementsByTagName("x").item(0).getTextContent();
-                        String yEndString = shapeElement.getElementsByTagName("y").item(0).getTextContent();   
+                    if (type.equals("cube")) {
+                        int size = Integer.parseInt(
+                                shapeElement.getElementsByTagName("size").item(0).getTextContent());
+                        listShape.add(this.CubeGenerator(type, x + 25, y + 25, size));
+                    } else if (type.equals("groupShape")) {
+                        String xEndString =
+                                shapeElement.getElementsByTagName("x").item(0).getTextContent();
+                        String yEndString =
+                                shapeElement.getElementsByTagName("y").item(0).getTextContent();
                         int xEnd = Integer.parseInt(xEndString);
                         int yEnd = Integer.parseInt(yEndString);
-                        listShape.add(this.GroupShapeGenerator(type, x+25, y+25, xEnd, yEnd, listShape));
-                    }
-                    else {
-                        listShape.add(this.AbstractShapeGenerator(type, x+25, y+25));
+                        listShape.add(this.GroupShapeGenerator(type, x + 25, y + 25, xEnd, yEnd,
+                                listShape));
+                    } else {
+                        listShape.add(this.AbstractShapeGenerator(type, x + 25, y + 25));
                     }
                 }
             }
